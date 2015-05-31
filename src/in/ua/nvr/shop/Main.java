efe27fb4b7b22922e734818c9d9329c62820ec0f
Georgiy2009 <nik2004@bigmir.net>
2015-05-31 16:22:58 +0300

    Initial commit

---
+++ src/in/ua/nvr/shop/Main.java
@@ -0,0 +1,16 @@
+package in.ua.nvr.shop;
+
+/** lb5
+ * Created by Олеся on 31.05.2015.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        new Main().run();
+    }
+
+    private void run() {
+
+    }
+
+}
---
+++ src/in/ua/nvr/shop/kids/Tovar.java
@@ -0,0 +1,13 @@
+package in.ua.nvr.shop.kids;
+
+/** Класс товар
+ * Created by Олеся on 31.05.2015.
+ * тип товара (мяч,конструктор ..),название, цена, возраст,примечание параметр(см,грам шт)
+ */
+public class Tovar {
+    String tip;
+    String name;
+    Integer cost;
+    Integer age; 
+
+}
